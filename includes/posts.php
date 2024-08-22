7f56325a54acc4da3173d6e5cf1e94891b590313
Michael Monetized <michaelmonetized@gmail.com>
2024-08-22 15:00:10 -0400

    maybe fixed dates

--- includes/posts.php
+++ includes/posts.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * Handles functionality, related to posts.
  * /includes/posts.php
  */
-class HLSocials_Posts {
+class HLSocials_Posts
+{
 
 	/**
 	 * Retrieve the available post types.
@@ -13,7 +15,8 @@ class HLSocials_Posts {
 	 *
 	 * @return array $post_types Retrieve the available post types.
 	 */
-	public static function get_post_types() {
+	public static function get_post_types()
+	{
 		$post_types = array();
 
 		$all_post_types = get_post_types(array(), 'objects');
@@ -36,18 +39,20 @@ class HLSocials_Posts {
 	 * @param int $parent ID of the parent entry.
 	 * @return int $total Number of entries that were inserted.
 	 */
-	public static function process_hierarchy($hierarchy = array(), $start_date = false, $post_type = 'socials', $post_status = 'future', $parent = 0) {
+	public static function process_hierarchy($hierarchy = array(), $start_date = false, $post_type = 'socials', $post_status = 'future', $parent = 0)
+	{
 		$start_date = $start_date ? $start_date : date('c', strtotime('+1 days'));
 
 		$total = 0;
+
 		foreach ($hierarchy as $hierarchy_entry) {
-			$next_time = strtotime("$start_date +$total days");
+			$next_time = $total == 0 ? strtotime("$start_date") : strtotime("$start_date +$total days");
+			$total = $total + 1;
 			$next_date = date('c', $next_time);
 
 			$id = self::insert($post_type, $hierarchy_entry['title'], $post_status, $parent, $next_date);
-			$total++;
 
-			if ( !empty($hierarchy_entry['children']) ) {
+			if (!empty($hierarchy_entry['children'])) {
 				$total += self::process_hierarchy($hierarchy_entry['children'], $next_date, $post_type, $post_status, $id);
 			}
 		}
@@ -67,14 +72,15 @@ class HLSocials_Posts {
 	 * @param int $parent ID of the parent post.
 	 * @return int $id The ID of the inserted post.
 	 */
-	public static function insert($post_type, $title, $post_status, $parent = 0, $date = false) {
-		if(!$date){
+	public static function insert($post_type, $title, $post_status, $parent = 0, $date = false)
+	{
+		if (!$date) {
 			$date = date('Y-m-d H:i:s', strtotime('+1 days'));
 		}
 
 		$content = $title;
 		$title_arr = explode(' ', $title);
-		$short_title = $title_arr[0].' '.$title_arr[1];
+		$short_title = $title_arr[0] . ' ' . $title_arr[1];
 
 		$id = wp_insert_post(array(
 			'post_type' => $post_type,
@@ -83,10 +89,9 @@ class HLSocials_Posts {
 			'post_parent' => $parent,
 			'post_status' => $post_status,
 			'post_date' => $date,
-			'post_date_gmt' => get_gmt_from_date( $date ),
+			'post_date_gmt' => get_gmt_from_date($date),
 		));
 
 		return $id;
 	}
-
 }
